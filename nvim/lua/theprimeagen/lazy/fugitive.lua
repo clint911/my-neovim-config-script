d502ea19804650af46059b3fdfa7c036cbb3ab4a
clint911 <clingyking007@gmail.com>
2025-05-13 17:05:25 +0300

    updating my neovim config for use in server

--- nvim/lua/theprimeagen/lazy/fugitive.lua
+++ nvim/lua/theprimeagen/lazy/fugitive.lua
@@ -27,7 +27,7 @@ return {
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+                vim.keymap.set("n", "<leader>gk", ":Git push -u origin ", opts);
             end,
         })
 
